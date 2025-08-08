0f9091e98d564ff0c346855adee7300178730341
lanaS silverio <lanasilverio373@gmail.com>
2025-08-07 22:22:29 -0300

    criaçao de projeto no visual studio

---
+++ 04_vetores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04_vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
