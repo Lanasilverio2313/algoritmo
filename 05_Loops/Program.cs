34e1a81c108b56469397113c888c85892d578592
lanaS silverio <lanasilverio373@gmail.com>
2025-08-08 21:43:30 -0300

    Loop while

---
+++ 05_Loops/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _05_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string [100];
+            string continuar = "S";
+            
+            //sera meu indice para atribuiçao no vetor
+            int contador = 0;
+
+            //Loop while
+            //Sintaxe: while(expessao booleana
+
+            while (continuar.ToUpper() == "S") // metado Toupper converte texto para maiusculo
+            {
+
+                Console.WriteLine("Digite o {0}ª nome:", contador + 1);
+                //Apped adiciona um item no vetor
+                nomes[contador] = Console.ReadLine();
+
+            
+                contador++;
+                Console.WriteLine("deseja continuar? (S/N)");
+                continuar = Console.ReadLine();
+
+                Console.WriteLine("nomes informados");
+                foreach (string str in nomes)
+                {
+                    // "!= significa diferente
+                    if (str != null)
+                {
+                        Console.WriteLine(str);
+                    }
+
+                        
+                    
+                }
+            }
+
+            }
+        }
+    }
