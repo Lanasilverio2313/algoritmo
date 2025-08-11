a46a301ef5a53a35b5e04686fa073f40b59e3f8f
lanaS silverio <lanasilverio373@gmail.com>
2025-08-11 20:15:48 -0300

    Projeto criaçao de Tabuada

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicando = 0, produto;
+            bool digitacaoValida = false;
+
+            //!negaçao: inverte o valor  booleano
+            //!false = true
+            // while (digitacoaValida== false)
+            while (!digitacaoValida) {
+
+                Console.WriteLine("digite um numero inteiro");
+                digitacaoValida = int.TryParse(Console.ReadLine(), out multiplicando);
+
+                //cosole.WriteLine(tabuada do {0}", multiplicador);
+                //Interpolaçao de string $
+                Console.WriteLine($"TABUADA DO {multiplicando}");
+                for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+                {
+                    produto = multiplicando * multiplicando;
+                    //console.WhiteLine("{0} x {1} = {2}", multiplicando, multiplicador produto);
+                    Console.WriteLine($"{multiplicando} x {multiplicador} = {produto}");
+                }
+            }
+
+        }
+    }
+}
